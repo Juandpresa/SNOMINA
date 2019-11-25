7148d09849383fc4c5e60033067aa744cc21bc8a
ALEONRAMOS <55206685+ALEONRAMOS@users.noreply.github.com>
2019-11-25 14:00:00 -0600

    Ddl prestaciones

--- SNOMINA/EscenariosQnta/Vista/wfrmEmpleado.aspx.designer.cs
+++ SNOMINA/EscenariosQnta/Vista/wfrmEmpleado.aspx.designer.cs
@@ -454,6 +454,15 @@ namespace EscenariosQnta {
         protected global::System.Web.UI.WebControls.DropDownList ddlTipoPago;
         
         /// <summary>
+        /// Control ddlPrestacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlPrestacion;
+        
+        /// <summary>
         /// Control lblAntiguedad.
         /// </summary>
         /// <remarks>
