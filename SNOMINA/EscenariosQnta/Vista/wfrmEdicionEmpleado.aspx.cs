0635e0346674b3fa06341a022e096fd9e038efbd
DEMEZ PRESA <demes_chivas@hotmail.com>
2019-12-11 12:29:54 -0600

    asignacion de empleado en datos generales edicion

--- SNOMINA/EscenariosQnta/Vista/wfrmEdicionEmpleado.aspx.cs
+++ SNOMINA/EscenariosQnta/Vista/wfrmEdicionEmpleado.aspx.cs
@@ -23,12 +23,93 @@ namespace EscenariosQnta.Vista
       if (!IsPostBack)
       {
         idEmp = (int)Session["IdEmpleado"];
+        ObtenEmpleadoras();
+        ObtenTipoEsquema();
+        ObtenSexo();
         SpInformacion();
 
 
-        ObtenTipoEsquema();
+        
       }
     }
+
+    protected void ObtenEmpleadoras()
+    {
+      try
+      {
+        DataTable dtEmpleadora = new DataTable();
+
+        dtEmpleadora = clsQuery.execQueryDataTable("SP_ObtenEmpleadoras");
+        ddlEmpleadora.Items.Clear();
+
+        if (dtEmpleadora.Rows.Count > 0)
+        {
+          ddlEmpleadora.DataSource = dtEmpleadora;
+          ddlEmpleadora.DataTextField = "Alias";
+          ddlEmpleadora.DataValueField = "idRazonSocial";
+          ddlEmpleadora.DataBind();
+        }
+
+        ddlEmpleadora.Items.Insert(0, new ListItem(">> Seleccione una Opcion <<", "-1"));
+
+      }
+      catch (Exception ex)
+      {
+        Mensaje("ERROR: " + ex.ToString(), CuadroMensaje.CuadroMensajeIcono.Error);
+      }
+    }
+
+    protected void ObtenClientes(int idEmpleadora)
+    {
+      try
+      {
+        DataTable dtCliente = new DataTable();
+
+        dtCliente = BLLEmpleado.ObtenClientes(idEmpleadora);
+        ddlCliente.Items.Clear();
+
+        if (dtCliente.Rows.Count > 0)
+        {
+          ddlCliente.DataSource = dtCliente;
+          ddlCliente.DataTextField = "Alias";
+          ddlCliente.DataValueField = "ClienteID";
+          ddlCliente.DataBind();
+        }
+
+        ddlCliente.Items.Insert(0, new ListItem(">> Seleccione una Opcion <<", "-1"));
+
+      }
+      catch (Exception ex)
+      {
+        Mensaje("ERROR: " + ex.ToString(), CuadroMensaje.CuadroMensajeIcono.Error);
+      }
+    }
+
+    protected void ObtenSexo()
+    {
+      try
+      {
+        DataTable dtSexo = new DataTable();
+
+        dtSexo = clsQuery.execQueryDataTable("SP_ObtenSexo");
+
+        if (dtSexo.Rows.Count > 0)
+        {
+          ddlSexo.DataSource = dtSexo;
+          ddlSexo.DataTextField = "Nombre";
+          ddlSexo.DataValueField = "SexoID";
+          ddlSexo.DataBind();
+        }
+
+        ddlSexo.Items.Insert(0, new ListItem(">> Seleccione una Opcion <<", "-1"));
+
+      }
+      catch (Exception ex)
+      {
+        Mensaje("ERROR: " + ex.ToString(), CuadroMensaje.CuadroMensajeIcono.Error);
+      }
+    }
+
     protected void ObtenTipoEsquema()
     {
       DataTable dtTipoEsquema = new DataTable();
@@ -43,6 +124,7 @@ namespace EscenariosQnta.Vista
         rbtTipoEsquema.DataBind();
       }
     }
+
     public void SpInformacion()
     {
       string[] es = new string[38];
@@ -55,19 +137,37 @@ namespace EscenariosQnta.Vista
           es[j] = dtEMP.Rows[0][j].ToString();
         }
       }
-      //ddlEmpleadora.Text = es[1].ToString();
-      ddlCliente.Text = es[2].ToString();
+      ddlEmpleadora.SelectedValue = es[1].ToString();
+      ObtenClientes(int.Parse(es[1].ToString()));
+      ddlCliente.SelectedValue = es[2].ToString();
       txtClave.Text = es[3].ToString();
       txtNombre.Text = es[4].ToString();
       txtPaterno.Text = es[5].ToString();
       txtMaterno.Text = es[6].ToString();
-      //ddlSexo.Text = es[7].ToString();
+      ddlSexo.SelectedValue = es[7].ToString();
       txtFechaNacimiento.Text = es[8].ToString();
       txtCurp.Text = es[9].ToString();
       txtRfc.Text = es[10].ToString();
       txtCorreo.Text = es[11].ToString();
       txtTelefonoMovil.Text = es[12].ToString();
       txtTelefonoLocal.Text = es[13].ToString();
+      if (es[14].ToString() == "1")
+      {
+        rbtTipoEsquema.Items[0].Selected = true;
+      }
+      else
+      {
+        rbtTipoEsquema.Items[1].Selected = true;
+      }
+     
+
+    }
+
+    public void Mensaje(string textomensaje, CuadroMensaje.CuadroMensajeIcono icono)
+    {
+      CuadroMensaje Mensaje = new CuadroMensaje(textomensaje, "MENSAJE", icono, CuadroMensaje.CuadroMensajeBoton.Ok, CuadroMensaje.CuadroMensajeEstilo.EstiloB);
+      Literal ltrMensaje = (Literal)this.Master.FindControl("ltrMensaje");
+      ltrMensaje.Text = Mensaje.Mostrar(this);
     }
   }
 }
